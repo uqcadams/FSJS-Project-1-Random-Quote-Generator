7073a2676766d36f16b9debce7b0f4acef25edab
Chris Adams <uqcadams@gmail.com>
2022-02-20 07:39:05 +1000

    Basic refactoring
    Added more developed JSDoc comments, implemented updated / streamlined randomHSL functionality, and moved dataset into separate file

---
+++ js/data.js
@@ -0,0 +1,82 @@
+/**
+ * An array of objects containing quote data.
+ * Quote objects are selected at random to be displayed in the DOM.
+ */
+const quotes = [
+  {
+    quote:
+      "You know as well as I do that fear only exists for one purpose... to be conquered.",
+    source: "Captain Kathryn Janeway",
+    citation: "Star Trek: Voyager",
+    episode: "Season 2, Episode 23",
+    year: "1996",
+  },
+  {
+    quote:
+      "You can use logic to justify almost anything. That's its power and its flaw.",
+    source: "Captain Kathryn Janeway",
+    citation: "Star Trek: Voyager",
+    episode: "Season 1, Episode 10",
+    year: "1995",
+  },
+  {
+    quote: "One voice can be stronger than a thousand voices.",
+    source: "Captain Kathryn Janeway",
+    citation: "Star Trek: Voyager",
+    episode: "Season 4, Episode 2",
+    year: "1997",
+  },
+  {
+    quote: "We're Starfleet officers. 'Weird' is part of the job.",
+    source: "Captain Kathryn Janeway",
+    citation: "Star Trek: Voyager",
+    episode: "Season 2, Episode 21",
+    year: "1996",
+  },
+  {
+    quote:
+      "There are three things to remember about being a starship captain: keep your shirt tucked in, go down with the ship, and never abandon a member of your crew.",
+    source: "Captain Kathryn Janeway",
+    citation: "Star Trek: Voyager",
+    episode: "Season 5, Episode 15",
+    year: "1999",
+  },
+  {
+    quote:
+      "I Have Never Subscribed To The Theory That Political Power Flows From The Barrel Of A Gun.",
+    source: "Captain Jean-Luc Picard",
+    citation: "Star Trek: The Next Generation",
+    episode: "Season 3, Episode 12",
+    year: "1990",
+  },
+  {
+    quote:
+      "The First Duty Of Every Starfleet Officer Is To The Truth. It Is The Guiding Principle Upon Which Starfleet Is Based.",
+    source: "Captain Jean-Luc Picard",
+    citation: "Star Trek: The Next Generation",
+    episode: "Season 5, Episode 19",
+    year: "1992",
+  },
+  {
+    quote: "I Do Not Fire On Defenseless People.",
+    source: "Captain Jean-Luc Picard",
+    citation: "Star Trek: The Next Generation",
+    episode: "Season 5, Episode 14",
+    year: "1992",
+  },
+  {
+    quote: "You Must Not Kneel To Me. I Do Not Deserve It.",
+    source: "Captain Jean-Luc Picard",
+    citation: "Star Trek: The Next Generation",
+    episode: "Season 3, Episode 4",
+    year: "1989",
+  },
+  {
+    quote:
+      "The First Speech Censured, The First Thought Forbidden, The First Freedom Denied...Chains Us All, Irrevocably.",
+    source: "Captain Jean-Luc Picard",
+    citation: "Star Trek: The Next Generation",
+    episode: "Season 4, Episode 21",
+    year: "1991",
+  },
+];
--- js/script.js
+++ js/script.js
@@ -1,131 +1,30 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1A Random Quote Generator
-******************************************/
-
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack communityhttps://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
-// Initialises an array containing quote objects
-const quotes = [
-  {
-      quote: "You know as well as I do that fear only exists for one purpose... to be conquered.",
-      source: "Captain Kathryn Janeway",
-      citation: "Star Trek: Voyager",
-      episode: "Season 2, Episode 23",
-      year: "1996"
-  },
-  {
-      quote: "You can use logic to justify almost anything. That's its power and its flaw.",
-      source: "Captain Kathryn Janeway",
-      citation: "Star Trek: Voyager",
-      episode: "Season 1, Episode 10",
-      year: "1995"
-  },
-  {
-      quote: "One voice can be stronger than a thousand voices.",
-      source: "Captain Kathryn Janeway",
-      citation: "Star Trek: Voyager",
-      episode: "Season 4, Episode 2",
-      year: "1997"
-  },
-  {
-      quote: "We're Starfleet officers. 'Weird' is part of the job.",
-      source: "Captain Kathryn Janeway",
-      citation: "Star Trek: Voyager",
-      episode: "Season 2, Episode 21",
-      year: "1996"
-  },
-  {
-      quote: "There are three things to remember about being a starship captain: keep your shirt tucked in, go down with the ship, and never abandon a member of your crew.",
-      source: "Captain Kathryn Janeway",
-      citation: "Star Trek: Voyager",
-      episode: "Season 5, Episode 15",
-      year: "1999"
-  },
-  {
-      quote: "I Have Never Subscribed To The Theory That Political Power Flows From The Barrel Of A Gun.",
-      source: "Captain Jean-Luc Picard",
-      citation: "Star Trek: The Next Generation",
-      episode: "Season 3, Episode 12",
-      year: "1990"
-  },
-  {
-      quote: "The First Duty Of Every Starfleet Officer Is To The Truth. It Is The Guiding Principle Upon Which Starfleet Is Based.",
-      source: "Captain Jean-Luc Picard",
-      citation: "Star Trek: The Next Generation",
-      episode: "Season 5, Episode 19",
-      year: "1992"
-  },
-  {
-      quote: "I Do Not Fire On Defenseless People.",
-      source: "Captain Jean-Luc Picard",
-      citation: "Star Trek: The Next Generation",
-      episode: "Season 5, Episode 14",
-      year: "1992"
-  },
-  {
-      quote: "You Must Not Kneel To Me. I Do Not Deserve It.",
-      source: "Captain Jean-Luc Picard",
-      citation: "Star Trek: The Next Generation",
-      episode: "Season 3, Episode 4",
-      year: "1989"
-  },
-  {
-      quote: "The First Speech Censured, The First Thought Forbidden, The First Freedom Denied...Chains Us All, Irrevocably.",
-      source: "Captain Jean-Luc Picard",
-      citation: "Star Trek: The Next Generation",
-      episode: "Season 4, Episode 21",
-      year: "1991"
-  }
-]
-
-
-
-
-
-/***
- * `getRandomQuote` function
-***/
-
+/**
+ * Function to randomly select and return a quote. Uses the Math.random() method and the length of the quotes array to randomly generate a number within the available range. This is further manipulated with the Math.floor() function to ensure that the random integer is a whole number within the possible range, and contains no decimals.
+ * @returns {object} - Returns a random quote object selected from the quotes array.
+ */
 const getRandomQuote = () => {
   // Generates a random number based on the length of the quotes array
-  let randomNumber = Math.floor(Math.random() * quotes.length); 
+  let randomNumber = Math.floor(Math.random() * quotes.length);
   // Stores a random quotes based on the index number generated
   let randomQuote = quotes[randomNumber];
   return randomQuote;
-}
+};
 
-/***
- * `getRandomColor` function
-***/
+/**
+ * A refactored random color generator function.
+ * Generates a random number between 0 and 360, which is then interpolated into an HSL expression. The random number passed as the hue value represents a degree or angle on the colour circle. Saturation and Lightness are reduced to 40% to improve contrast against white text.
+ * @returns {string} - a randomly generated hsl colour
+ */
+const getRandomHue = () => {
+  let randomHue = Math.floor(Math.random() * 360);
+  let randomColor = `hsl(${randomHue}, 40%, 40%)`;
+  return randomColor;
+};
 
-const getRandomDC = () => {
-  // Generators a random decimal code between 0 and 255
-  let randomNumber = Math.floor(Math.random() * 255); 
-  return randomNumber;
-}
-
-const getRandomRGB = () => {
-  // Generates an rgb string with three random decimal codes
-  let randomRGB = `rgb(${getRandomDC()}, ${getRandomDC()}, ${getRandomDC()})`;
-  return randomRGB;
-}
-
-
-
-
-/***
- * `printQuote` function
-***/
-
+/**
+ * Prepares and displays a random quote on the DOM. A random quote is selected from the array of quote objects, and then dynamically injected into an existing HTML template. Additional quote details are conditionally rendered if present in the dataset.
+ */
 const printQuote = () => {
-
   // Stores the random quote object in a variable
   let randomQuote = getRandomQuote();
 
@@ -138,49 +37,43 @@ const printQuote = () => {
       <p class="source"> ${randomQuote.source}
       `;
 
+  // Include citation if provided
   if (randomQuote.citation) {
     quoteHtml += `
       <span class="citation">${randomQuote.citation}</span>
       `;
   }
-  // Additional object property inserted (for episode reference)
-  if (randomQuote.year) {
+  // Include episode reference if provided
+  if (randomQuote.episode) {
     quoteHtml += `
       <span class="year">${randomQuote.episode}</span>
-      `;    
+      `;
   }
+  // Include publication year if provided
   if (randomQuote.year) {
     quoteHtml += `
       <span class="year">${randomQuote.year}</span>
-      `;    
+      `;
   }
 
+  // Inserts element closing tag
   quoteHtml += `</p>`;
 
-  // Updates the innertHTML of the quote box using dynamic quote content
-  document.getElementById('quote-box').innerHTML = quoteHtml; 
+  // Updates the innerHTML of the quote box using dynamic quote content
+  document.getElementById("quote-box").innerHTML = quoteHtml;
 
-  // Updates the background colour with a random RGB
-  document.querySelector('body').style.backgroundColor = `${getRandomRGB()}`;
-}
+  // Updates the background colour with a random HSL value
+  document.querySelector("body").style.backgroundColor = `${getRandomHue()}`;
+};
 
+/**
+ * Autoplay interval to change quote every 5 seconds
+ */
+setInterval(printQuote, 5000);
 
-/***
- * `printQuote` interval
-***/
-
-const autoPlay = (duration) => {
-  // Sets an interval on the printQuote function
-  setInterval(printQuote, duration);
-}
-
-// Duration set to 5s (5000ms);
-autoPlay(5000);
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
-
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+/**
+ * Implements additional click event handling to manually change quote
+ */
+document
+  .getElementById("load-quote")
+  .addEventListener("click", printQuote, false);
